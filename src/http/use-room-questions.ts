e4cd9b03f79de659f4ca0be33018caf024e71c18
Carlos Andrade <carlosdestroy1@gmail.com>
2025-07-12 15:17:33 -0300

    feat: add recording audio on questions

---
+++ src/http/use-room-questions.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query"
+import type { GetRoomsQuestionsAPIResponse } from "./types/get-room-questions-respose"
+
+export function useRoomQuestions(roomId: string){
+    return useQuery({
+    queryKey: ['get-questions', roomId],
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3333/rooms/${roomId}/questions`)
+      const result: GetRoomsQuestionsAPIResponse = await response.json()
+
+      return result
+    },
+  })
+}
